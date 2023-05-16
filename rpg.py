c9b5675369e74fed0ea1fbbbd17917edc7a1f7bd
uzun <uzuzun11@gmail.com>
2023-05-16 17:14:13 +0300

    update

--- rpg.py
+++ rpg.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-characters= list(string.ascii_letters + string.digits + "!@#$%^&*")
+characters= list(string.ascii_letters + string.digits + "_-+")
 
 def generate_password():
     password_length = int(input("How long would you like your password to be? "))
